ff20e78f101cbe6f52dcebc713ba1701865b12eb
exp111 <theexp111@gmail.com>
2017-11-01 15:25:06 +0100

    (Hopefully) Added correct way for chat capture
    Not tested tho

--- PlethoraModem.lua
+++ PlethoraModem.lua
@@ -28,29 +28,33 @@ parallel.waitForAny(
 	end,
 	function()
 		while true do
-			os.pullEvent("getTime()")
-			local day = manipulator.getDay()
-			local gameTime = manipulator.getTime();
-        	local hours = gameTime / 1000 + 6
-        	local minutes = (gameTime % 1000) * 60 / 1000
-        	local ampm = "AM"
-        	if (hours >= 12) then
-            	hours -= 12 
-				ampm = "PM"
-        	end
+			local event, message, pattern = os.pullEvent("chat_capture")
+			if (message = getTime()) then
+				local day = manipulator.getDay()
+				local gameTime = manipulator.getTime();
+        			local hours = gameTime / 1000 + 6
+        			local minutes = (gameTime % 1000) * 60 / 1000
+        			local ampm = "AM"
+        			if (hours >= 12) then
+            				hours -= 12 
+					ampm = "PM"
+        			end
  
-        	if (hours >= 12) then
-            	hours -= 12
- 				ampm = "AM"
-        	end
+        			if (hours >= 12) then
+            				hours -= 12
+ 					ampm = "AM"
+        			end
  
-       	 	if (hours == 0) then hours = 12 end
+       	 			if (hours == 0) then 
+					hours = 12 
+				end
  
-        	local mm = "0" .. minutes
-        	mm = strsub(mm, strlen(mm) - 2, strlen(mm));
+        			local mm = "0" .. minutes
+        			mm = strsub(mm, strlen(mm) - 2, strlen(mm));
  
-        	local finaltime = hours .. ":" .. mm .. " " .. ampm;
-			manipulator.tell("It's " .. finaltime .. " on the " .. day .. ". day.")
+        			local finaltime = hours .. ":" .. mm .. " " .. ampm;
+				manipulator.tell("It's " .. finaltime .. " on the " .. day .. ". day.")
+			end
 		end
 	end
 )
