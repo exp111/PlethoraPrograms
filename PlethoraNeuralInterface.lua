af762270d6dc0c212f6151d0a629311116d45617
exp111 <theexp111@gmail.com>
2017-11-05 13:19:54 +0100

    Some Changes
    -Added Control and Info Text (modular; informs you which features you can use)
    -Fixed Program not working without Introspection Module

--- PlethoraNeuralInterface.lua
+++ PlethoraNeuralInterface.lua
@@ -15,26 +15,42 @@ local modemPort = 1337
 local notificationPosStyle = "relativeDirection"
 
 --KEYS:
-local launchUpKey = keys.e
-local launchDirectionKey = keys.f
-local mobScanKey = keys.numPad9
-local useSpamKey = keys.numPad8
-local aimbotKey = keys.numPad7
-local noFallKey = keys.numPad6
-local hoverKey = keys.numPad5
-local oreScanKey = keys.numPad4
+local launchUpKey = {}
+launchUpKey.key = keys.e
+launchUpKey.keyname = "E"
+local launchDirectionKey = {}
+launchDirectionKey.key = keys.f
+launchDirectionKey.keyname = "F"
+local mobScanKey = {}
+mobScanKey.key = keys.numPad9
+mobScanKey.keyname = "NumPad9"
+local useSpamKey = {}
+useSpamKey.key = keys.numPad8
+useSpamKey.keyname = "NumPad8"
+local aimbotKey = {}
+aimbotKey.key = keys.numPad7
+aimbotKey.keyname = "NumPad7"
+local noFallKey = {}
+noFallKey.key = keys.numPad6
+noFallKey.keyname = "NumPad6"
+local hoverKey = {}
+hoverKey.key = keys.numPad5
+hoverKey.keyname = "NumPad5"
+local oreScanKey = {}
+oreScanKey.key = keys.numPad4
+oreScanKey.keyname = "NumPad4"
 
 ---------------------------------------------------
 local modules = peripheral.find("neuralInterface")
 if not modules then
-    error("Must have a neural interface", 0)
+	error("Must have a neural interface", 0)
 end
 
 if preferredMethod == "modem" then
 	--Modem
 	modem = peripheral.find("modem")
 	if not modem then
-    	error("Must have a modem", 0)
+		error("Must have a modem", 0)
 	end
 	modem.open(modemPort)
 	if modem.isOpen(modemPort) == false then
@@ -94,12 +110,12 @@ hover.event = "hover"
 
 local mobLookup = {}
 for i = 1, #mobNames do
-    mobLookup[mobNames[i]] = true
+	mobLookup[mobNames[i]] = true
 end
 
 local oreLookup = {}
 for i = 1, #oreNames do
-    oreLookup[oreNames[i]] = true
+	oreLookup[oreNames[i]] = true
 end
 
 --Helper Functions
@@ -116,9 +132,9 @@ function tell(message)
 end
 
 local function toggle(array)
- 	array.enabled = not array.enabled
-    tell(array.name .. " was set to " .. tostring(array.enabled))
-    if array.enabled then 
+	array.enabled = not array.enabled
+	tell(array.name .. " was set to " .. tostring(array.enabled))
+	if array.enabled then 
 		os.queueEvent(array.event)
 	end
 end
@@ -190,149 +206,157 @@ local function findNearest(array)
 	end
 	return nearestMob
 end
+
+local function clearScreen()
+	term.setBackgroundColor(colours.black)  -- Set the background colour to black.
+	term.clear()                            -- Paint the entire display with the current background colour.
+	term.setCursorPos(1,1)                  -- Move the cursor to the top left position.
+end
  
 -- Start Main Loop
 parallel.waitForAny(
-    --- This loop just pulls user input. It handles a couple of function keys, as well as
-    --- setting the "hover" field to true/false.
-    function()
-        while true do
-            local event, key = os.pullEvent("key")
-            if key == launchUpKey then
-                if hasKinetic then
-                	modules.launch(0, -90, 3)
+	--- This loop just pulls user input. It handles a couple of function keys, as well as
+	--- setting the "hover" field to true/false.
+	function()
+		while true do
+			local event, key = os.pullEvent("key")
+			if key == launchUpKey.key then
+				if hasKinetic then
+					modules.launch(0, -90, 3)
 				else
 					tell("No Kinetic Module installed; Feature Disabled")
 				end
-			elseif key == launchDirectionKey then
-               if hasKinetic then
-                	modules.launch(meta.yaw, meta.pitch, 3)
+			elseif key == launchDirectionKey.key then
+			   if hasKinetic then
+					modules.launch(meta.yaw, meta.pitch, 3)
 				else
 					tell("No Kinetic Module installed; Feature Disabled")
 				end
-            elseif key == mobScanKey then
+			elseif key == mobScanKey.key then
 				if hasSensor then
-                	toggle(mobscan)
+					toggle(mobscan)
 				else
 					tell("No Entity Sensor installed; Feature Disabled")
 				end
-            elseif key == useSpamKey then
+			elseif key == useSpamKey.key then
 				if hasKinetic then
-                	toggle(use)
+					toggle(use)
 				else
 					tell("No Kinetic Module installed; Feature Disabled")
 				end
-            elseif key == aimbotKey then
+			elseif key == aimbotKey.key then
 				if hasSensor and hasKinetic then
-                	toggle(aimbot)
+					toggle(aimbot)
 				else
 					tell("Feature needs to have Kinetic Module and Entity Sensor installed; Feature Disabled")
 				end
-            elseif key == noFallKey then
-                if hasKinetic and hasScanner and hasIntrospection then
+			elseif key == noFallKey.key then
+				if hasKinetic and hasScanner and hasIntrospection then
 					toggle(nofall)
 				else
 					tell("Feature needs to have Kinetic-, Introspection Module and Block Scanner installed; Feature Disabled")
 				end
-            elseif key == hoverKey then
+			elseif key == hoverKey.key then
 				if hasKinetic and hasIntrospection then
-                	toggle(hover)
+					toggle(hover)
 				else
 					tell("Feature needs to have Kinetic and Introspection Module installed; Feature Disabled")
 				end
-            elseif key == oreScanKey then
+			elseif key == oreScanKey.key then
 				if hasScanner then
-                	toggle(orescan)
+					toggle(orescan)
 				else
 					tell("No Block Scanner installed; Feature Disabled")
 				end
-            end
-        end
-    end,
-	function() -- Update Meta Data
-		while true do
-			meta = modules.getMetaOwner()
+			end
 		end
 	end,
-    function() --Mob Scanner
-        while true do
-            if mobscan.enabled then
-                local mobs = modules.sense()
+	function() -- Update Meta Data
+		while true do
+			if (hasIntrospection) then
+				meta = modules.getMetaOwner()
+			end
+		end
+	end,
+	function() --Mob Scanner
+		while true do
+			if mobscan.enabled then
+				local mobs = modules.sense()
  
-   				local candidates = {}
-    			for i = 1, #mobs do
-        			local mob = mobs[i]
-        			if mobLookup[mob.name] then
-            			candidates[#candidates + 1] = mob
-        			end
-    			end
+				local candidates = {}
+				for i = 1, #mobs do
+					local mob = mobs[i]
+					if mobLookup[mob.name] then
+						candidates[#candidates + 1] = mob
+					end
+				end
  
-    			if #candidates > 0 then
-            		if #candidates > 3 then
+				if #candidates > 0 then
+					if #candidates > 3 then
 						local mobString = candidates[1].name
-                		for i=2, #candidates do
-                			mobString = mobString .. ", " .. candidates[i].name
-               			end
+						for i=2, #candidates do
+							mobString = mobString .. ", " .. candidates[i].name
+						end
 						if (string.len(mobString) > 70) then mobString = "Too many Mobs" end
-                		tell(tostring(#candidates) .. " Mobs found near you: " .. mobString)
-            		else
-        				for i=1, #candidates do
-            				local mob = candidates[i]
+						tell(tostring(#candidates) .. " Mobs found near you: " .. mobString)
+					else
+						for i=1, #candidates do
+							local mob = candidates[i]
 							if mob.y < 5 and mob.y > -5 then
 								local pos = getDirection(mob)
 								local mobpos = getPosition(mob)
-            					tell("Mob Scanner | " .. i .. ": " .. mob.name .. " found at " .. pos.x .. mobpos.x .. pos.y .. mobpos.y .. pos.z .. mobpos.z)
-    						end
-    					end
-    				end
-    			end
+								tell("Mob Scanner | " .. i .. ": " .. mob.name .. " found at " .. pos.x .. mobpos.x .. pos.y .. mobpos.y .. pos.z .. mobpos.z)
+							end
+						end
+					end
+				end
 				sleep(2)
 			else
-            	os.pullEvent(mobscan.event)
-        	end
+				os.pullEvent(mobscan.event)
+			end
 		end
-    end,
-    function() -- Use Spam
-        while true do
-            if use.enabled then
-                if (modules.use(1,"main") == false) then
-                    tell("No Block to use")
+	end,
+	function() -- Use Spam
+		while true do
+			if use.enabled then
+				if (modules.use(1,"main") == false) then
+					tell("No Block to use")
 					use = false
 					tell("Use Spam was set to " .. tostring(use))
-                end
+				end
 				sleep(0.5)
-            else
-                os.pullEvent(use.event)
-            end
-        end
-    end,
+			else
+				os.pullEvent(use.event)
+			end
+		end
+	end,
 	function() --Aimbot
-        while true do
-            if aimbot.enabled then
+		while true do
+			if aimbot.enabled then
 				local mobs = modules.sense()
  
-   				local candidates = {}
-    			for i = 1, #mobs do
-        			local mob = mobs[i]
-        			if mobLookup[mob.name] then
-            			candidates[#candidates + 1] = mob
-        			end
-    			end
+				local candidates = {}
+				for i = 1, #mobs do
+					local mob = mobs[i]
+					if mobLookup[mob.name] then
+						candidates[#candidates + 1] = mob
+					end
+				end
 
 				if #candidates > 0 then
-        			local mob = findNearest(candidates)
+					local mob = findNearest(candidates)
 					look(mob)
 					sleep(0.2)
-    			end
-            else
-                os.pullEvent(aimbot.event)
-            end
-        end
-    end,
+				end
+			else
+				os.pullEvent(aimbot.event)
+			end
+		end
+	end,
 	function() -- No Fall
-        while true do
-            if nofall.enabled then
-                local blocks = modules.scan()
+		while true do
+			if nofall.enabled then
+				local blocks = modules.scan()
 				for y = 0, -8, -1 do
 					-- Scan from the current block downwards
 					local block = blocks[1 + (8 + (8 + y)*17 + 8*17^2)]
@@ -344,11 +368,11 @@ parallel.waitForAny(
 					break
 					end
 				end
-            else
-                os.pullEvent(nofall.event)
-            end
-        end
-    end,
+			else
+				os.pullEvent(nofall.event)
+			end
+		end
+	end,
 	function() -- Hover
 		while true do
 			if hover.enabled then
@@ -370,32 +394,97 @@ parallel.waitForAny(
 		end
 	end,
 	function() -- Ore Scanner
-        while true do
-            if orescan.enabled then
-                local ores = modules.scan()
+		while true do
+			if orescan.enabled then
+				local ores = modules.scan()
  
-   				local candidates = {}
-    			for i = 1, #ores do
-        			local ore = ores[i]
-        			if oreLookup[ore.name] then
-            			candidates[#candidates + 1] = ore
-        			end
-    			end
+				local candidates = {}
+				for i = 1, #ores do
+					local ore = ores[i]
+					if oreLookup[ore.name] then
+						candidates[#candidates + 1] = ore
+					end
+				end
  
-    			if #candidates > 0 then
-        			for i=1, #candidates do
-            			local ore = candidates[i]
+				if #candidates > 0 then
+					for i=1, #candidates do
+						local ore = candidates[i]
 						local pos = getDirection(ore)
 						local orepos = getPosition(ore)
-            			tell("Ore Scanner | " .. i .. ": " .. ore.name .. " found at " .. pos.x .. orepos.x .. pos.y .. orepos.y .. pos.z .. orepos.z)
-    				end
-    			else
+						tell("Ore Scanner | " .. i .. ": " .. ore.name .. " found at " .. pos.x .. orepos.x .. pos.y .. orepos.y .. pos.z .. orepos.z)
+					end
+				else
 					tell("Ore Scanner | No Ores found")
 				end
 				orescan.enabled = false
 			else
-            	os.pullEvent(orescan.event)
-        	end
+				os.pullEvent(orescan.event)
+			end
 		end
-    end
+	end,
+	function() -- Show Text 
+		while true do
+			clearScreen()
+			--Launch
+			if (hasKinetic) then
+				term.setTextColor(colors.green)
+				print("Launch Upwards: Press " .. launchUpKey.keyname .. ".")
+				print("Launch in Direction: Press " .. launchDirectionKey.keyname .. ".")
+			else
+				term.setTextColor(colors.red)
+				print("Launch Upwards: Needs Kinetic Module.")
+				print("Launch in Direction: Needs Kinetic Module.")
+			end
+			--Mob Scanner
+			if (hasSensor) then
+				term.setTextColor(colors.green)
+				print("Toggle Mob Scanner: Press " .. mobScanKey.keyname .. ".")
+			else
+				term.setTextColor(colors.red)
+				print("Toggle Mob Scanner: Needs Entity Sensor.")
+			end
+			--Use Spam
+			if (hasKinetic) then
+				term.setTextColor(colors.green)
+				print("Toggle Use Spam: Press " .. useSpamKey.keyname .. ".")
+			else
+				term.setTextColor(colors.red)
+				print("Toggle Use Spam: Needs Kinetic Module.")
+			end
+			--Aimbot
+			if (hasKinetic and hasSensor) then
+				term.setTextColor(colors.green)
+				print("Toggle Aimbot: Press " .. aimbotKey.keyname .. ".")
+			else
+				term.setTextColor(colors.red)
+				print("Toggle Aimbot: Needs Kinetic Module and Entity Sensor.")
+			end
+			--NoFall
+			if (hasKinetic and hasScanner and hasIntrospection) then
+				term.setTextColor(colors.green)
+				print("Toggle No Fall Damage: Press " .. noFallKey.keyname .. ".")
+			else
+				term.setTextColor(colors.red)
+				print("Toggle No Fall Damage: Needs Kinetic Module, Block Scanner and Introspection Module.")
+			end
+			--Hoever
+			if (hasKinetic and hasIntrospection) then
+				term.setTextColor(colors.green)
+				print("Toggle Hover: Press " .. hoverKey.keyname .. ".")
+			else
+				term.setTextColor(colors.red)
+				print("Toggle Hover: Needs Kinetic- and Introspection Module.")
+			end
+			--Block Scan
+			if (hasScanner) then
+				term.setTextColor(colors.green)
+				print("Activate Ore Scan: Press " .. oreScanKey.keyname)
+			else
+				term.setTextColor(colors.red)
+				print("Activate Ore Scan: Needs Block Scanner.")
+			end
+			--sleep
+			sleep(5)
+		end
+	end
 )
